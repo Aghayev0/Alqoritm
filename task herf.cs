a133a367aeeb121c61bfd81ff9284c1c1f715376
Aghayev0 <tu6k6wzh2@code.edu.az>
2022-10-30 22:39:40 +0400

    salam

---
+++ task herf.cs
@@ -0,0 +1,11 @@
+﻿string herf;
+Console.WriteLine("Herf daxil edin");
+herf = Console.ReadLine();
+
+switch (herf)
+{
+    case "a": Console.Write("Azerbaycan dilini secdiniz"); break;
+    case "r": Console.Write("Rus dilini secdiniz"); break;
+    case "A": Console.Write("AZ"); break;
+    case "R": Console.Write("RU"); break;
+}
